d03cc040c98b0b696e695ee3ab19e642698959cd
Bryce Ito <bryceito@amazon.com>
2019-12-19 11:10:22 -0800

    Adding awsComponent, making things more generic

--- src/webviews/activation.ts
+++ src/webviews/activation.ts
@@ -7,7 +7,7 @@ import * as vscode from 'vscode'
 import { TelemetryNamespace } from '../shared/telemetry/telemetryTypes'
 import { registerCommand } from '../shared/telemetry/telemetryUtils'
 import { createReactWebview } from './reactLoader'
-import { InvokerState } from './tsx/interfaces/invokerInterfaces'
+import { InvokerState } from './tsx/interfaces/invoker'
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
     // invoker
@@ -22,22 +22,26 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
                 onDidDisposeFunction: myDisposal,
                 context,
                 initialState: {
-                    region: {
-                        value: 'us-weast-1',
-                        isValid: true
+                    validityFields: {
+                        region: {
+                            value: 'us-weast-1',
+                            isValid: true
+                        },
+                        lambda: {
+                            value: '',
+                            isValid: true
+                        },
+                        payload: {
+                            value: '{"whoop": "there it is"}',
+                            isValid: false
+                        },
+                        template: {
+                            value: "not your mama's template",
+                            isValid: true
+                        }
                     },
-                    lambda: {
-                        value: '',
-                        isValid: true
-                    },
-                    payload: {
-                        value: '{"whoop": "there it is"}',
-                        isValid: false
-                    },
-                    template: {
-                        value: "not your mama's template",
-                        isValid: true
-                    }
+                    booleans: {},
+                    strings: {}
                 }
             }),
         telemetryName: {
@@ -48,25 +52,29 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 }
 
 async function myMessageHandler(message: InvokerState, postMessageFn: (event: InvokerState) => Thenable<boolean>) {
-    vscode.window.showInformationMessage(message.payload.value)
+    vscode.window.showInformationMessage(message.validityFields.payload.value)
     vscode.window.showInformationMessage('posting message!')
     const result = await postMessageFn({
-        region: {
-            value: 'modified region!',
-            isValid: true
+        validityFields: {
+            region: {
+                value: 'modified region!',
+                isValid: true
+            },
+            lambda: {
+                value: 'modified lambda!',
+                isValid: true
+            },
+            payload: {
+                value: 'modified payload!',
+                isValid: false
+            },
+            template: {
+                value: 'modified template!',
+                isValid: true
+            }
         },
-        lambda: {
-            value: 'modified lambda!',
-            isValid: true
-        },
-        payload: {
-            value: 'modified payload!',
-            isValid: false
-        },
-        template: {
-            value: 'modified template!',
-            isValid: true
-        }
+        booleans: {},
+        strings: {}
     })
     vscode.window.showInformationMessage(`Posted message!: ${result.toString()}`)
 }
--- src/webviews/tsx/invokeRemote.tsx
+++ src/webviews/tsx/invokeRemote.tsx
@@ -6,76 +6,59 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import { generateDefaultValidityField, ValidityField, vscode } from '../utils'
-import { InvokerProps, InvokerState } from './interfaces/invokerInterfaces'
+import { AwsComponent } from './components/awsComponent'
+import { generateDefaultValidityField, VsCode, VsCodeReactWebviewProp } from './interfaces/common'
+import { InvokerState } from './interfaces/invoker'
 
-declare const vscode: vscode<InvokerState>
+declare const vscode: VsCode<InvokerState>
 
 function generateDefaultInvokerState(): InvokerState {
     return {
-        lambda: generateDefaultValidityField(),
-        payload: generateDefaultValidityField(),
-        region: generateDefaultValidityField(),
-        template: generateDefaultValidityField()
+        validityFields: {
+            lambda: generateDefaultValidityField(),
+            payload: generateDefaultValidityField(),
+            region: generateDefaultValidityField(),
+            template: generateDefaultValidityField()
+        },
+        booleans: {},
+        strings: {}
     }
 }
 
-export class Invoker extends React.Component<InvokerProps, InvokerState> {
-    public constructor(props: InvokerProps) {
-        super(props)
-
-        const existingState = this.props.vscode.getState() as InvokerState
-        if (existingState) {
-            this.state = existingState
-        } else {
-            this.state = generateDefaultInvokerState()
-        }
-    }
-
-    public componentDidMount() {
-        console.log('mounted!')
-        window.addEventListener('message', event => this.overwriteStateWithMessage((event.data as any) as InvokerState))
-    }
-
-    public componentWillUnmount() {
-        window.removeEventListener('message', event =>
-            this.overwriteStateWithMessage((event.data as any) as InvokerState)
-        )
-    }
-
+export class Invoker extends AwsComponent<VsCodeReactWebviewProp<InvokerState>, InvokerState> {
     public render() {
         return (
             <div>
                 <input
                     name="region"
                     placeholder="region"
-                    value={this.state.region.value}
-                    onChange={e => this.onChange(e)}
-                    className={`${this.state.region.isValid ? '' : 'invalid'}`}
+                    value={this.state.validityFields.region.value}
+                    onChange={e => this.onValidityFieldChange(e)}
+                    className={`${this.state.validityFields.region.isValid ? '' : 'invalid'}`}
                 />
                 <br />
                 <input
                     name="lambda"
                     placeholder="lambda"
-                    value={this.state.lambda.value}
-                    onChange={e => this.onChange(e)}
-                    className={`${this.state.lambda.isValid ? '' : 'invalid'}`}
+                    value={this.state.validityFields.lambda.value}
+                    onChange={e => this.onValidityFieldChange(e)}
+                    className={`${this.state.validityFields.lambda.isValid ? '' : 'invalid'}`}
                 />
                 <br />
                 <input
                     name="template"
                     placeholder="template"
-                    value={this.state.template.value}
-                    onChange={e => this.onChange(e)}
-                    className={`${this.state.template.isValid ? '' : 'invalid'}`}
+                    value={this.state.validityFields.template.value}
+                    onChange={e => this.onValidityFieldChange(e)}
+                    className={`${this.state.validityFields.template.isValid ? '' : 'invalid'}`}
                 />
                 <br />
                 <textarea
                     name="payload"
                     placeholder="JSON Payload"
-                    value={this.state.payload.value}
-                    onChange={e => this.onChange(e)}
-                    className={`${this.state.payload.isValid ? '' : 'invalid'}`}
+                    value={this.state.validityFields.payload.value}
+                    onChange={e => this.onValidityFieldChange(e)}
+                    className={`${this.state.validityFields.payload.isValid ? '' : 'invalid'}`}
                 />
                 <br />
                 <button onClick={e => this.onSubmit(e)}>Submit!</button>
@@ -83,29 +66,15 @@ export class Invoker extends React.Component<InvokerProps, InvokerState> {
         )
     }
 
-    private overwriteStateWithMessage(message: InvokerState) {
-        this.setState(message)
-    }
-
-    private onChange(event: React.ChangeEvent) {
-        // enforce that the element has value and name fields
-        const target = event.target as HTMLInputElement
-        // enforce that the name is a key
-        const name = target.name as keyof InvokerState
-        const stateForName: ValidityField = this.state[name]
-        stateForName.value = target.value
-        this.setState(({
-            [name]: stateForName
-        } as any) as Pick<InvokerState, keyof InvokerState>)
-        this.props.vscode.setState(this.state)
-    }
-
     private onSubmit(event: React.MouseEvent) {
         try {
-            JSON.parse(this.state.payload.value)
+            JSON.parse(this.state.validityFields.payload.value)
             this.props.vscode.postMessage(this.state)
         } catch (e) {}
     }
 }
 
-ReactDOM.render(<Invoker vscode={vscode} />, document.getElementById('reactApp'))
+ReactDOM.render(
+    <Invoker vscode={vscode} defaultState={generateDefaultInvokerState()} />,
+    document.getElementById('reactApp')
+)
